f3783ad27840840b3b404b0c485c2412702ff911
kittipong <kittipong3495@outlook.com>
2020-08-05 10:53:41 +0700

    update done

--- src/TodoList.js
+++ src/TodoList.js
@@ -1,10 +1,47 @@
 import React, { useState, useEffect } from 'react';
+import "./todolist.css";
 import axios from 'axios';
-import './todolist.css';
 import { faTrash,faPen } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-function TodoApp() {
-  const [data, setData] = useState({ todoitem: [] });
+
+function Todo({ todo, index, completeTodo, removeTodo }) {
+  return (
+    <div
+      className="list-group-item"
+      style={{ textDecoration: todo.isCompleted ? "line-through" : "" }}
+    >
+      <input type="checkbox" className="check_box" defaultChecked={todo.isCompleted} onClick={() => completeTodo(index)}/>
+      {todo.todolist_text}
+      <a onClick={() => removeTodo(todo.id)} className="close"><FontAwesomeIcon icon={faTrash}/></a>
+      <a className="close"> <FontAwesomeIcon icon={faPen} /></a>
+    </div>
+  );
+}
+
+function TodoForm({ addTodo }) {
+  const [value, setValue] = useState("");
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (!value) return;
+    addTodo(value);
+    setValue("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className="form_todo">
+      <input
+        type="text"
+        className="form-control" placeholder="Input task name then tap Enter add"
+        value={value}
+        onChange={e => setValue(e.target.value)}
+      />
+    </form>
+  );
+}
+
+function TodoList() {
+  const [todos, setTodos] = useState([]);
 
   useEffect(() => {
     gettodolist();
@@ -15,60 +52,57 @@ function TodoApp() {
       'https://todolisttest-heroku.herokuapp.com/todo/todolist',
     );
 
-    setData({ 
-      todoitem: result.data.rows 
-    });
+    setTodos(result.data.rows);
   };
 
-  const addItem = e => {
-    const txt_list = e.target.value;
-    if (e.key === 'Enter') {
-      axios.post(`https://todolisttest-heroku.herokuapp.com/todo/todolist`, { todolist_text:txt_list })
-      .then(res => {
-        gettodolist();
-      })
+  const addTodo = todolist_text => {
+    axios.post(`https://todolisttest-heroku.herokuapp.com/todo/todolist`, { todolist_text:todolist_text })
+    .then(res => {
+      gettodolist();
+    })
+  };
+
+  const completeTodo = index => {
+    const newTodos = [...todos];
+    newTodos[index].isCompleted = !newTodos[index].isCompleted;
+    const data = { 
+      todolist_text:newTodos[index].todolist_text,
+      isCompleted:newTodos[index].isCompleted 
     }
+    axios.put(`https://todolisttest-heroku.herokuapp.com/todo/todolist/${newTodos[index]['id']}`,data)
+    .then(res => {
+      setTodos(newTodos);
+    })
   };
 
-  const deleteItem = (id) => {
+  const removeTodo = id => {
     axios.delete(`https://todolisttest-heroku.herokuapp.com/todo/todolist/${id}`)
     .then(res => {
       gettodolist();
     })
-  }
+  };
 
-  function doneToClass(status){
-    var doneClass = '';
-    if(status === 1) {
-      doneClass = 'done test';
-    }else{
-    doneClass = 'test';
-    }
- 
-    return doneClass ;
-  }
-      return (
+  return (
+    <div className="app"> 
+      <div className="container">
         <div className="card">
-        <div className="card-body">
-          <h5 className="card-title text-center">My Todo</h5>
-            <input type="text" className="form-control" placeholder="Input task name then tap Enter add" onKeyPress={addItem}/>
-            <ul className="ul_custom">
-              {data.todoitem.map(item => (
-                <li className="list-group-item " key={item.id}>
-                  <div className="item_todo ">
-                    <input type="checkbox" className="form-check-input " defaultChecked={item.status}/>
-                    <label className={doneToClass(item.status)}>{item.todolist_text}</label>
-                    <a className="close" onClick={() => deleteItem(item.id)}> <FontAwesomeIcon icon={faTrash}/></a>
-                    <a className="close"> <FontAwesomeIcon icon={faPen} /></a>
-                  </div>
-                </li>     
-              ))}
-            </ul>
+          <div className="card-body">
+            <h5 className="card-title text-center header_todo">My Todo</h5>
+            <TodoForm addTodo={addTodo} />
+            {todos.map((todo, index) => (
+              <Todo
+                key={index}
+                index={index}
+                todo={todo}
+                completeTodo={completeTodo}
+                removeTodo={removeTodo}
+              />
+            ))}
+          </div>
         </div>
       </div>
+    </div>
+  );
+}
 
-      )
-  }
-  
-  export default TodoApp;
-  
+export default TodoList;
